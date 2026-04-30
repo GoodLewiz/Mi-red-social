7a7991778edc18cc27baaf5822016b8f7491c1b9
Luis Felipe Salazar <goodlewiz25@gmail.com>
2026-04-30 12:46:00 -0500

    Se agregan fotos a los usuarios

--- script.js
+++ script.js
@@ -5,24 +5,30 @@ let publicaciones = [
     usuario: "Ana",
     contenido: "Hola mundo 🌍",
     likes: 0,
-    comentarios: []
+    imagen: "https://i.pravatar.cc/40?u=Carlos",
+    comentarios : []
+
   },
   {
     usuario: "Carlos",
     contenido: "Mi primera publicación 😎",
     likes: 0,
-    comentarios: []
+    imagen:  "https://i.pravatar.cc/40?u=Ana",
+    comentarios : []
   }
 ];
 
 // LOGIN SIMULADO
 function login() {
   let user = document.getElementById("username").value;
-  if (user === "") return alert("Escribe un usuario");
+  let pass = document.getElementById("password").value;
 
+  if (user === "") return alert("Escribe un usuario");
+  if(pass === "") return alert("Escribe una contraseña")
   usuarioActual = user;
   document.getElementById("login").classList.add("hidden");
   document.getElementById("feed").classList.remove("hidden");
+
 
   renderPosts();
 }
@@ -37,16 +43,26 @@ function renderPosts() {
     div.className = "card";
 
     div.innerHTML = `
-      <strong>${post.usuario}</strong>
+     <div style="display:flex; align-items:center; gap:10px;">
+        <img src=${post.imagen}"alt="Foto de ${post.usuario}" width="40" height="40" style="border-radius:50%;">
+        <strong>${post.usuario}</strong>
+     </div>
+
+
       <p>${post.contenido}</p>
       <button onclick="like(${index})">❤️ ${post.likes}</button>
       <br><br>
       <input placeholder="Comentar..." id="coment-${index}">
       <button onclick="comentar(${index})">Enviar</button>
       <ul>
-        ${post.comentarios.map(c => `<li>${c}</li>`).join("")}
-      </ul>
-    `;
+      ${post.comentarios.map(c => `
+      <li style="display:flex; align-items:center; gap:8px; margin-top:8px;">
+        <img src="${c.imagen}" alt="Foto de ${c.usuario}" width="30" height="30" style="border-radius:50%;">
+        <span><strong>${c.usuario}:</strong> ${c.texto}</span>
+      </li>
+    `).join("")}
+  </ul>
+`;
 
     contenedor.appendChild(div);
   });
@@ -65,7 +81,11 @@ function comentar(index) {
 
   if (texto === "") return;
 
-  publicaciones[index].comentarios.push(usuarioActual + ": " + texto);
+  publicaciones[index].comentarios.push({
+   usuario : usuarioActual,
+   texto :texto,
+   imagen :  "https://i.pravatar.cc/40?u="
+  });
   input.value = "";
 
   renderPosts();
